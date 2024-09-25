f00b6d4dfb5fd8041805e21a797fb49bc2613e8b
Edgar Leyva Corona <elc.uvm@hotmail.com>
2024-09-25 11:02:58 -0600

    WIP Remove unused code

--- WordFinderTests/GlobalUsings/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using WordFinderAPI.Services;  // Importa el servicio donde está la clase WordFinder
-global using Xunit;                   // Importa xUnit para las pruebas
-global using System.Collections.Generic;
